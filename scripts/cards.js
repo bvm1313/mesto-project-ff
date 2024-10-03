a291ff180a7716afbcbaf8f4cb4e91b175a26ae9
Vladimir Biktimirov <bvm1313@yandex.ru>
2024-10-04 01:33:31 +0500

    с помощью функций и метода forEach добавил карточки на страницу, первая итерация

--- scripts/cards.js
+++ scripts/cards.js
@@ -2,25 +2,31 @@ const initialCards = [
     {
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      alt:  "гора Архыз",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      alt: "Небольшое озеро в снежному лесу",
     },
     {
       name: "Иваново",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      alt: "Многоэажки в городе",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      alt: "гора в Камчатке",
     },
     {
       name: "Холмогорский район",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      alt: "Железная дорога в лесу",
     },
     {
       name: "Байкал",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      alt: "озеро Байкал в зимой",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
-// @todo: Функция создания карточки
+const createCard = (item, deleteCard) => {
+  const elementCard = cardTemplate.querySelector(".card").cloneNode(true);
+  elementCard.querySelector(".card__title").textContent = item.name; 
+  elementCard.querySelector(".card__image").src = item.link;
+  elementCard.querySelector(".card__image").alt = item.alt;  
+  const deleteButton =  elementCard.querySelector(".card__delete-button");
+  deleteButton.addEventListener('click', deleteCard);
+  return elementCard;
+};
 
-// @todo: Функция удаления карточки
+function deleteCard(evt) {
+    const listItem = evt.target.closest(".card");
+    listItem.remove();
+} ;
+  
+initialCards.forEach((item) => {
+  const card = createCard(item, deleteCard);
+  placesList.append(card);
+});
 
-// @todo: Вывести карточки на страницу
